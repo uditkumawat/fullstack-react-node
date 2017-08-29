1048116d6597a0985b22b13bbdb4ba8da163867c
cl-macmini-122 <udit.kumawat@click-labs.com>
2017-08-29 10:38:20 +0530

    exact paramter in route for corret matching

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -17,7 +17,8 @@ const App = () =>{
         <div>
            <BrowserRouter>
                 <div>
-                    <Route path="/" component = {Landing}/>
+                    <Route exact={true} path="/" component = {Landing}/>
+                    <Route path="/surveys" component={Dashboard}/>
                 </div>
             </BrowserRouter>
         </div>
